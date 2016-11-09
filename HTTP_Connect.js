6097d4b58b48e11d54617b183981f18ae5695118
Mike <mikegabriel1404@gmail.com>
2016-11-09 15:32:53 -0500

    Test 4 fois

---
+++ HTTP_Connect.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mike on 09/11/16.
+ */
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'mike',
+    password: '',
+    database: 'tour_finance'
+});
+
+
