2b20e9752b6afc2b2d07ae003a2193d64d0d3918
Mike <mikegabriel1404@gmail.com>
2016-11-10 16:00:56 -0500

    j'ai enleve la mention UNIQUE a city puisque plusieurs bands iront a la meme ville,  et j'ai imbriquer les FOR et mis une en commentaire pour eviter den avoir trop, mais la seule chose qu'il reste a faire c'est faire en sorte que city arrete de se repeter apres 100 fois!

--- HTTP_Connect.js
+++ HTTP_Connect.js
@@ -31,43 +31,45 @@ connection.connect(function (err) {
                     console.log("added band " + courant);
                 }
             }(i));
+        for (var j = 0; j < 100; j++) {           //script for table "city"
+
+            var city_name = "city " + j;
+            var demain = new Date(2016, 0, 20);
+            var tour_date = new Date(demain.setDate(demain.getDate()+j));
+            connection.query(
+                {
+                    sql: "insert into city values(null, ?, ?)",
+                    values: [city_name, tour_date]
+                },
+
+                function (courant) {
+                    return function (err, rows) {
+                        if (err) throw err;
+
+                        console.log("added city " + courant);
+                    }
+                }(j));
+            //for (var k = 0; k < 100; k++) {           //script for finances "finances"
+
+                var spendings = Math.random()*1000;
+                var revenues = Math.random()*1000;
+                connection.query(
+                    {
+                        sql: "insert into finances values(null, ?, ?, ?, ?)",
+                        values: [band_name, tour_date, spendings, revenues]
+                    },
+
+                    function (courant) {
+                        return function (err, rows) {
+                            if (err) throw err;
+
+                            console.log("added finances " + courant);
+                        }
+                    });
+
+        }
     }
-    for (var j = 0; j < 100; j++) {           //script for table "city"
 
-        var city_name = "city " + j;
-        var demain = new Date(2016, 0, 20);
-        var tour_date = new Date(demain.setDate(demain.getDate()+j));
-        connection.query(
-            {
-                sql: "insert into city values(null, ?, ?)",
-                values: [city_name, tour_date]
-            },
 
-            function (courant) {
-                return function (err, rows) {
-                    if (err) throw err;
-
-                    console.log("added city " + courant);
-                }
-            }(j));
-    }
-    for (var k = 0; k < 100; k++) {           //script for finances "finances"
-
-        var spendings = Math.random()*1000;
-        var revenues = Math.random()*1000;
-        connection.query(
-            {
-                sql: "insert into finances values(null, ?, ?, ?, ?)",
-                values: [band_name, tour_date, spendings, revenues]
-            },
-
-            function (courant) {
-                return function (err, rows) {
-                    if (err) throw err;
-
-                    console.log("added finances " + courant);
-                }
-            }(k));
-    }
     connection.end();
 });
--- SQL_tables_create.sql
+++ SQL_tables_create.sql
@@ -1,12 +1,12 @@
 # Script par Erick Paquin et Michael Gabriel le 3 Novembre 2016.
 #
 # create user and database
-create user 'tour_manager'@'localhost' identified by 'abcd';
+#create user 'tour_manager'@'localhost' identified by 'abcd';
 drop database if exists tour_finance;
 create database tour_finance;
 
 use tour_finance;
-grant all privileges on *.* to 'tour_manager'@'localhost';
+#grant all privileges on *.* to 'tour_manager'@'localhost';
 
 # create tables
 create table band(
@@ -17,7 +17,7 @@ financial_status varchar(256)
 
 create table city(
 id int primary key auto_increment,
-city_name varchar(256) unique, index(city_name),
+city_name varchar(256), index(city_name),
 tour_date date, index(tour_date)
 # band_pay decimal(6,2), index(band_pay)
 );
