f7582b23475db0c4fed764dc1501dc956ce6f9b0
Mike <mikegabriel1404@gmail.com>
2016-11-16 17:12:53 -0500

    rajout de mysql a la connex

--- http_server.js
+++ http_server.js
@@ -2,12 +2,25 @@
  * Created by Michael Gabriel and Erick Paquin on 16/11/16.
  */
 
+
+
 var http = require('http');
+var mysql = require('mysql');
 
 const PORT=8080;
 
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'tour_manager',
+        password: 'abcd',
+        database: 'tour_finance'
+    });
+    connection.connect(function (err) {
+        if (err) throw err;
+
+    });
 }
 
 var server = http.createServer(handleRequest);
