3893b25dd00a9347d7623b7a2dd51b12731f9459
Mike1404 <Hotdog123>
2016-11-25 21:43:09 -0500

    not much

---
+++ HTTP_Connect.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Michael Gabriel et Erick Paquin on 09/11/16.
+ */
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'tour_manager',
+    password: 'abcd',
+    database: 'tour_finance'
+});
+
+connection.connect(function (err) {
+
+    if (err) throw err;
+
+    for (var i = 0; i < 5; i++) {           //script for table "band"
+
+        var band_name = "band " + i;
+        connection.query(
+            {
+                sql: "insert into band values(null, ?, null)",
+                values: [band_name]
+            });
+        for (var j = 0; j < 100; j++) {           //script for table "city"
+
+            var city_name = "city " + j;
+            var demain = new Date(2016, 0, 20);
+            var tour_date = new Date(demain.setDate(demain.getDate()+j));
+            connection.query(
+                {
+                    sql: "insert into city values(null, ?, ?)",
+                    values: [city_name, tour_date]
+                });
+             //for (var k = 0; k < 100; k++) {           //script for finances "finances"
+
+                var spendings = Math.random()*1000;
+                var revenues = Math.random()*1000;
+                connection.query(
+                    {
+                        sql: "insert into finances values(null, ?, ?, ?, ?)",
+                        values: [band_name, tour_date, spendings, revenues]
+                    });
+
+        }
+    }
+    connection.end();
+});
