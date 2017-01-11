c61fad2bc10e3e310cba35c0578bd0fe4fabed91
Mike <mikegabriel1404@gmail.com>
2017-01-11 15:27:57 -0500

    THATSIT THE POPULATE + DELETE

--- FINAL_VERSIONS/DB_populate_FINAL.js
+++ FINAL_VERSIONS/DB_populate_FINAL.js
@@ -12,85 +12,102 @@ var connection = mysql.createConnection({
     database: 'tour_finance'
 });
 
-connection.connect(function (err, rows) {
+connection.connect(function (err) {
 
     if (err) throw err;
 
-    for (var i = 1; i < 6; i++) {           //script for table "band"
-
-        var band_name = "band" + i;
-
-        connection.query(
-            {
-                sql: "insert into band values(null, ?, null)",
-                values: [band_name]
-            });
-
-    }
-
-
-    for (var j = 1; j < 21; j++) {           //script for table "city" & "finance"
-
-        var city_name = "city" + j;
-        var demain = new Date(2016, 0, 20);
-        var tour_date = new Date(demain.setDate(demain.getDate() + j)).toLocaleDateString();
-        connection.query(
-            {
-                sql: "insert into city values(null, ?, ?)",
-                values: [city_name, tour_date]
-            });
-
-    }
-
-    for (var k = 1; k < 6; k++) {
-
-        connection.query(
-            {
-                sql: "select id from band where band_name = ?",
-                values: ["band" + k]
-            }, function (err, rows) {
-                bandid = rows[0].id;
-                getdateid(bandid);
-            });
-    }
-
-    function getdateid(theval) {
-        var bandid = theval;
-
-
-        for (var l = 1; l < 21; l++) {
-
+    connection.query(
+        {
+            sql: "delete from finances"
+        }, function () {
             connection.query(
                 {
-                    sql: "select id from city where city_name = ?",
-                    values: ["city" + l]
-                }, function (err, rows) {
-                    var dateid = rows[0].id;
-                    insertfinance(bandid, dateid);
+                    sql: "delete from city"
+                }, function () {
+                    connection.query(
+                        {
+                            sql: "DELETE FROM band"
+
+                        }, function () {
+
+                            for (var i = 1; i < 6; i++) {           //script for table "band"
+
+                                var band_name = "band" + i;
+
+                                connection.query(
+                                    {
+                                        sql: "insert into band values(null, ?, null)",
+                                        values: [band_name]
+                                    });
+                            }
+
+                            for (var j = 1; j < 21; j++) {           //script for table "city" & "finance"
+
+                                var city_name = "city" + j;
+                                var demain = new Date(2016, 0, 20);
+                                var tour_date = new Date(demain.setDate(demain.getDate() + j)).toLocaleDateString();
+
+                                connection.query(
+                                    {
+                                        sql: "insert into city values(null, ?, ?)",
+                                        values: [city_name, tour_date]
+                                    });
+
+                            }
+
+                            for (var k = 1; k < 6; k++) {
+
+                                connection.query(
+                                    {
+                                        sql: "select id from band where band_name = ?",
+                                        values: ["band" + k]
+                                    }, function (err, rows) {
+                                        bandid = rows[0].id;
+                                        getdateid(bandid);
+                                    });
+                            }
+
+                            function getdateid(theval) {
+
+                                var bandid = theval;
+
+                                for (var l = 1; l < 21; l++) {
+
+                                    connection.query(
+                                        {
+                                            sql: "select id from city where city_name = ?",
+                                            values: ["city" + l]
+                                        }, function (err, rows) {
+
+                                            var dateid = rows[0].id;
+
+                                            insertfinance(bandid, dateid);
+                                        });
+                                }
+                            }
+
+                            var insertedFinance = 0;
+
+                            function insertfinance(bandid, dateid) {
+
+                                var spendings = Math.random() * 1000;
+                                var revenues = Math.random() * 1000;
+
+                                connection.query(
+                                    {
+                                        sql: "insert into finances values(null, ?, ?, ?, ?)",
+                                        values: [bandid, dateid, spendings, revenues]
+                                    }, function (err) {
+                                        if (err) throw err;
+                                        insertedFinance++;
+                                        if (insertedFinance == 100) {
+                                            connection.end();
+                                        }
+
+                                    });
+                            }
+                        });
                 });
-        }
-    }
-
-    var insertedFinance = 0;
-
-    function insertfinance(bandid, dateid) {
-        var spendings = Math.random() * 1000;
-        var revenues = Math.random() * 1000;
-        var thebandid = bandid;
-        var thedateid = dateid;
-        connection.query(
-            {
-                sql: "insert into finances values(null, ?, ?, ?, ?)",
-                values: [thebandid, thedateid, spendings, revenues]
-            }, function (err, rows) {
-                if (err) throw err;
-                insertedFinance++;
-                if (insertedFinance == 500) {
-                    connection.end();
-                }
-
-            });
-    }
-
-
+        });
 });
+
